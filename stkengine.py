1b8e76999b986ba91a4d9c6753474e30703d74f0
Conor McFerren <cnmcferren@gmail.com>
2020-01-28 11:15:48 -0500

    Create function for STK

---
+++ stkengine.py
@@ -0,0 +1,33 @@
+import parse
+from stkhelper import application, scenario, satellite, areatarget
+
+def CalculateAccess(filename):
+    atDataList = parse.ParseFile('TargetList.csv')
+
+    app = application.Application()
+    scene = scenario.Scenario(app, 'Scheduling', '+24hrs')
+    sat = satellite.Satellite(scene, 'MOCI', 25544)
+
+    ats = []
+    for i in range(len(atDataList)):
+        at = areatarget.AreaTarget(scene,
+                               atDataList[i][0].replace(' ','_').replace('.','').replace('(','').replace(')',''),
+                               atDataList[i][1:4])
+        ats.append(at)
+    
+    groundStation = areatarget.AreaTarget(scene,'GroundStation',
+					[(33.9487,-83.3754),(33.9487,-83.3754)])
+    groundStation.SetElevationConstraint(25)
+
+    ats.append(groundStation)
+
+    outputFile = open('Access.csv','w')
+    for i in range(len(ats)):
+        access = sat.GetAccess(ats[i])
+        try:
+            for n in range(len(access)):
+                string = access[n][0] + "," + access[n][1] + "," + ats[i].name + '\n'
+                outputFile.write(string)
+        except Exception:
+            pass
+    outputFile.close()
