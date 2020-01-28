0c1a4137eaefb31785e8e1870260f4c56acc070d
W. Conor McFerren <cnmcferren@gmail.com>
2020-01-27 22:00:51 -0500

    changes

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ sat = satellite.Satellite(scene, 'MOCI', 25544)
 ats = []
 for i in range(len(atDataList)):
     at = areatarget.AreaTarget(scene,
-                               atDataList[i][0],
+                               atDataList[i][0].replace(' ','_').replace('.','').replace('(','').replace(')',''),
                                atDataList[i][1:4])
     ats.append(at)
     
-groundStation = areatarget.AreaTarget('GroundStation',
+groundStation = areatarget.AreaTarget(scene,'GroundStation',
                                       [(33.9487,-83.3754)])
 groundStation.SetElevationConstraint(25)
 
@@ -23,8 +23,11 @@ ats.append(groundStation)
 outputFile = open('Access.csv','w')
 for i in range(len(ats)):
     access = sat.GetAccess(ats[i])
-    for n in range(len(access)):
-        string = access[0] + "," + access[1] + "," + ats[i].name + '\n'
-        outputFile.write(string)
+    try:
+    	for n in range(len(access)):
+        	string = access[n][0] + "," + access[n][1] + "," + ats[i].name + '\n'
+        	outputFile.write(string)
+    except Exception:
+	pass
         
 outputFile.close()
